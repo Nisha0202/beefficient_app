7d3deeb88babd0e62484cf4b82aa04577c3289dd
Nisha0202 <nishajabatunnessa@gmail.com>
2025-01-15 17:43:38 +0600

    Page transfer

---
+++ app/src/main/java/com/example/beefficientapp/CreateTaskActivity.java
@@ -0,0 +1,14 @@
+package com.example.beefficientapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_task);
+    }
+}
--- app/src/main/java/com/example/beefficientapp/MainActivity.java
+++ app/src/main/java/com/example/beefficientapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.beefficientapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the floating action button by its ID
+        Button fab = findViewById(R.id.fab);
+
+        // Set a click listener on the button
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to CreateActivity
+                Intent intent = new Intent(MainActivity.this, CreateTaskActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
